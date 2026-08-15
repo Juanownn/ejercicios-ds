118855680ba52708ab78214ad5cfbcff8e9ecb0e
Juano Riquelme <riquelme.juan261@gmail.com>
2026-08-15 16:41:26 -0300

    ejercicio 5b listo

---
+++ ejercicio-5b/main.py
@@ -0,0 +1,16 @@
+passwordCorrecta = "Hola123"
+
+if __name__ == "__main__":
+    intento = 0
+    while intento < 3:
+        password = str(input("Ingrese la contraseña: "))
+        if password == passwordCorrecta:
+            print("Contraseña correcta")
+            break
+        else: 
+            intento += 1
+            print(f"Contraseña incorrecta. Intentos restantes: {3 - intento}")
+
+    if intento == 3:
+        print("Intentos terminados. Cerrando...")
+    
