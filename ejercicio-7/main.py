4c1a3af211c01c7696bf158eaa92a534f80c4ef8
riquelme.juan261 <riquelme.juan261@gmail.com>
2026-08-16 21:38:06 -0300

    ejercicio 7 listo

--- ejercicio-7/main.py
+++ ejercicio-7/main.py
@@ -1,2 +1,15 @@
-def analizar_temperaturas(lista):
-    return ()
+def analizar_temperaturas(registros):
+    temps = (max(registros), min(registros), sum(registros) / len(registros))
+    return (temps)
+
+if __name__ == "__main__":
+    temperaturas = []
+    seguir = True
+    while seguir:
+        temp = int(input("Ingresa una temperatura, 0 para salir: "))
+        if temp != 0:
+            temperaturas.append(temp)
+        else:
+            seguir = False
+    if len(temperaturas) != 0:
+        print(analizar_temperaturas(temperaturas))
